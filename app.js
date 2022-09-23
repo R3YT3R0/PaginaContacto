6221489b670a6ffe32540613143f3765ed974cd4
reynier <xreyniervx@gmail.com>
2022-09-23 05:43:44 -0400

    pagina contacto responsive

---
+++ app.js
@@ -0,0 +1,7 @@
+function alertInfoform(){
+          var nombreuser = document.getElementById("nombreuser").value
+          var emailuser = document.getElementById("emailuser").value
+          var mensajeuser = document.getElementById("mensajeuser").value
+
+          alert("Hola"+" "+ nombreuser + " " + "Vas a enviar un Mensaje Desde:" +" " + emailuser  );
+}
