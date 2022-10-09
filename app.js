23d41f3dfed0904525ff00dc07d9f01c06183b31
reynier <xreyniervx@gmail.com>
2022-10-09 06:34:17 -0400

    Nueva pagina contacto

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-function alertInfoform(){
-          var nombreuser = document.getElementById("nombreuser").value
-          var emailuser = document.getElementById("emailuser").value
-          var mensajeuser = document.getElementById("mensajeuser").value
-
-          alert("Hola"+" "+ nombreuser + " " + "Vas a enviar un Mensaje Desde:" +" " + emailuser  );
-}
+Swal.fire({
+          title: "Bienvenido"
+})
