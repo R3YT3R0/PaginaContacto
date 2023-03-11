c8f19a9c67facb6d6e3e6be5fa785aa73c404696
reynier <xreyniervx@gmail.com>
2023-03-10 22:54:15 -0400

    Nueva version

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-Swal.fire({
-          title: "Bienvenido"
-})
+document.getElementById("Cancel").addEventListener("click", resetForm);
+
+function resetForm() {
+  var Form = document.getElementById("FormularioSeccion2");
+
+  Form.reset();
+}
